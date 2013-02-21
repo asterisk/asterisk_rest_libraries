77b56d689d885cd89411e884898325c0d376a58b
Erin Spiceland <espiceland@digium.com>
2013-02-21 15:46:28 -0600

    Switch all tabs to spaces.

--- python/asteriskpy/channel.py
+++ python/asteriskpy/channel.py
@@ -1,77 +1,77 @@
 class Channel:
-	def __init__(self):
-		"""Initialize the Channel object."""
-		self.id = 1
+    def __init__(self):
+        """Initialize the Channel object."""
+        self.id = 1
 
 
-	def get_id(self):
-		"""Return the Channel object's id."""
-		return self.id
+    def get_id(self):
+        """Return the Channel object's id."""
+        return self.id
 
 
-	def delete(self):
-		"""Delete the Channel."""
-		is_success = True
-		return is_success
+    def delete(self):
+        """Delete the Channel."""
+        is_success = True
+        return is_success
 
 
-	def reject(self):
-		"""Reject the Channel."""
-		is_success = True
-		return is_success
+    def reject(self):
+        """Reject the Channel."""
+        is_success = True
+        return is_success
 
 
-	def answer(self):
-		"""Answer the Channel."""
-		is_success = True
-		return is_success
+    def answer(self):
+        """Answer the Channel."""
+        is_success = True
+        return is_success
 
 
-	def hangup(self):
-		"""Hang the Channel up."""
-		is_success = True
-		return is_success
+    def hangup(self):
+        """Hang the Channel up."""
+        is_success = True
+        return is_success
 
 
-	def mute(self):
-		"""Mute the Channel."""
-		is_success = True
-		return is_success
+    def mute(self):
+        """Mute the Channel."""
+        is_success = True
+        return is_success
 
 
-	def unmute(self):
-		"""Unmute the Channel."""
-		is_success = True
-		return is_success
+    def unmute(self):
+        """Unmute the Channel."""
+        is_success = True
+        return is_success
 
 
-	def record(self):
-		"""Initiate a new recording on the Channel."""
-		recording = Recording()
-		return recording
+    def record(self):
+        """Initiate a new recording on the Channel."""
+        recording = Recording()
+        return recording
 
 
-	def dial(self):
-		"""Dial."""
-		is_success = True
-		return is_success
+    def dial(self):
+        """Dial."""
+        is_success = True
+        return is_success
 
 
-	def add_event_handler(self, event_name, handler):
-		"""Add an event handler for Stasis events on this object.
+    def add_event_handler(self, event_name, handler):
+        """Add an event handler for Stasis events on this object.
 
-		For general events, use Asterisk.add_event_handler instead.
+        For general events, use Asterisk.add_event_handler instead.
 
-		"""
-		pass
+        """
+        pass
 
 
-	def remove_event_handler(self, event_name, handler):
-		"""Remove an event handler for Stasis events on this object.
+    def remove_event_handler(self, event_name, handler):
+        """Remove an event handler for Stasis events on this object.
 
-		For general events, use Asterisk.remove_event_handler instead.
+        For general events, use Asterisk.remove_event_handler instead.
 
-		"""
-		pass
+        """
+        pass
 
 
--- python/asteriskpy/endpoint.py
+++ python/asteriskpy/endpoint.py
@@ -1,27 +1,27 @@
 class Endpoint:
-	def __init__(self):
-		"""Initialize the Endpoint object."""
-		self.id = 1
+    def __init__(self):
+        """Initialize the Endpoint object."""
+        self.id = 1
 
 
-	def get_id(self):
-		"""Return the Endpoint object's id."""
-		return self.id
+    def get_id(self):
+        """Return the Endpoint object's id."""
+        return self.id
 
 
-	def add_event_handler(self, event_name, handler):
-		"""Add an event handler for Stasis events on this object.
+    def add_event_handler(self, event_name, handler):
+        """Add an event handler for Stasis events on this object.
 
-		For general events, use Asterisk.add_event_handler instead.
+        For general events, use Asterisk.add_event_handler instead.
 
-		"""
-		pass
+        """
+        pass
 
 
-	def remove_event_handler(self, event_name, handler):
-		"""Remove an event handler for Stasis events on this object.
+    def remove_event_handler(self, event_name, handler):
+        """Remove an event handler for Stasis events on this object.
 
-		For general events, use Asterisk.remove_event_handler instead.
+        For general events, use Asterisk.remove_event_handler instead.
 
-		"""
-		pass
+        """
+        pass
