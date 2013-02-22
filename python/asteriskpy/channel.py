f1994e275fea16d9f02b62cde1a31e2e8e89d9e5
Erin Spiceland <espiceland@digium.com>
2013-02-21 21:29:49 -0600

    Add copyright notice; conform to pep8

--- python/asteriskpy/channel.py
+++ python/asteriskpy/channel.py
@@ -1,3 +1,21 @@
+"""
+ Copyright (C) 2013 Digium, Inc.
+
+ Erin Spiceland <espiceland@digium.com>
+
+ See http://www.asterisk.org for more information about
+ the Asterisk project. Please do not directly contact
+ any of the maintainers of this project for assistance;
+ the project provides a web site, mailing lists and IRC
+ channels for your use.
+
+ This program is free software, distributed under the terms of
+ the GNU General Public License Version 2. See the LICENSE file
+ at the top of the source tree.
+
+"""
+
+
 class Channel:
     def __init__(self):
         """Initialize the Channel object."""
--- python/test_asteriskpy.py
+++ python/test_asteriskpy.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+"""
+ Copyright (C) 2013 Digium, Inc.
+
+ Erin Spiceland <espiceland@digium.com>
+
+ See http://www.asterisk.org for more information about
+ the Asterisk project. Please do not directly contact
+ any of the maintainers of this project for assistance;
+ the project provides a web site, mailing lists and IRC
+ channels for your use.
+
+ This program is free software, distributed under the terms of
+ the GNU General Public License Version 2. See the LICENSE file
+ at the top of the source tree.
+
+"""
 
 import sys
 from asteriskpy import AsteriskPy, Endpoint, Channel, Bridge, Recording
@@ -9,16 +25,16 @@ PORT = '8088'
 
 
 def main(argv):
-   ast = AsteriskPy(host=HOST, port=PORT)
-   result = ast.get_info()
-   print "Asterisk status is %s" % (result)
+    ast = AsteriskPy(host=HOST, port=PORT)
+    result = ast.get_info()
+    print "Asterisk status is %s" % (result)
 
-   endpoint = Endpoint()
-   channel = Channel()
-   bridge = Bridge()
-   recording = Recording()
+    endpoint = Endpoint()
+    channel = Channel()
+    bridge = Bridge()
+    recording = Recording()
 
-   return 0
+    return 0
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv) or 0)
