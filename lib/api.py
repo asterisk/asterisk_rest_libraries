f9af3ddf85dd4553209c3c348171020e72ccb44a
Erin Spiceland <espiceland@digium.com>
2013-03-03 17:26:32 -0600

    Move param string construction into python.py

--- lib/api.py
+++ lib/api.py
@@ -32,6 +32,7 @@ class APIClassMethod():
         self.lang_tools = param_obj['lang_tools']
         self.method_params = ['self']
         self.required_id = False
+        self.param_obj = None
         self.api_summary = ''
         self.method_summary = ''
         self.method_name = ''
@@ -54,58 +55,19 @@ class APIClassMethod():
         self.api_summary = summary
 
     def set_parameters(self, param_obj):
-        """Construct an array of required and optional method parameters
-        in the format: name_type[_list][=None]
-             objectId_number
-             query='both'
-
-        """
-        t_attr = get_file_content('%s/templates/method_params_attr.proto'
-                                  % self.lang)
-        for p in param_obj:
-            if p['name'] == "%sId" % (self.file_name):
-                continue
-
-            param_name = "%s_%s" % (p['name'], p['dataType'])
-
-            if 'allowMultiple' in p and p['allowMultiple']:
-                param_name = param_name + "_list"
-
-            param_name = re.sub('([A-Z]{1,1})', r'_\1', param_name)
-            param_name = param_name.lower()
-            attr = re.sub('\{ATTR_NAME\}', param_name, t_attr)
-            attr = re.sub('\{ATTR_ORIG_NAME\}', p['name'], attr)
-            self.param_lines.append(attr)
-
-            if 'defaultValue' in p:
-                p['defaultValue'] = "'%s'" % (p['defaultValue'])
-            else:
-                p['defaultValue'] = None
-
-            param = "%s=%s" % (param_name, p['defaultValue'])
-
-            self.method_params.append(param)
-
-    def get_param_string(self):
-        """Return the string of all method parameters for method definition"""
-        return ', '.join(self.method_params)
+        self.param_obj = param_obj
 
     def construct_file_contents(self):
         """Construct and return the contents of the method definition"""
         t_method = get_file_content('%s/templates/method_def.proto'
                                     % self.lang)
         t_method = re.sub('\{API_METHOD_NAME\}', self.method_name, t_method)
-        t_method = re.sub('\{PARAMS\}', self.get_param_string(), t_method)
-        params = ["'%s'" % (self.path),
-                  "http_method='%s'" % (self.http_method)]
-        if self.method_name:
-            params.append("api_method='%s'" % (self.method_name))
-        if self.method_params:
-            params.append("parameters=params")
-        if self.required_id:
-            params.append("object_id=self.object_id")
 
-        t_method = re.sub('\{API_CALL_PARAMS\}', ', '.join(params), t_method)
+        string = self.lang_tools.make_param_string(self)
+        t_method = re.sub('\{PARAMS\}', string, t_method)
+
+        call_param_string = self.lang_tools.make_api_call_params(self)
+        t_method = re.sub('\{API_CALL_PARAMS\}', call_param_string, t_method)
         method_comment = self.lang_tools.make_method_comment(
             self.api_summary, self.method_summary)
         t_method = re.sub('\{METHOD_COMMENTS\}', method_comment, t_method)
--- lib/python.py
+++ lib/python.py
@@ -15,6 +15,7 @@ detailed in the the LICENSE file at the top of the source tree.
 
 """
 import re
+from utils import get_file_content
 
 
 def make_filename(name):
@@ -55,12 +56,41 @@ def make_method_name(name, class_name):
     return name
 
 
-def make_param_string(params):
+def make_param_string(method):
     """Make the string containing the parameter definition for each method
-    in a class.
+    in a class
 
     """
-    pass
+    t_attr = get_file_content('%s/templates/method_params_attr.proto'
+                              % method.lang)
+    if method.param_obj is None:
+        return 'self'
+
+    for p in method.param_obj:
+        if p['name'] == "%sId" % (method.file_name):
+            continue
+
+        param_name = "%s_%s" % (p['name'], p['dataType'])
+
+        if 'allowMultiple' in p and p['allowMultiple']:
+            param_name = param_name + "_list"
+
+        param_name = re.sub('([A-Z]{1,1})', r'_\1', param_name)
+        param_name = param_name.lower()
+        attr = re.sub('\{ATTR_NAME\}', param_name, t_attr)
+        attr = re.sub('\{ATTR_ORIG_NAME\}', p['name'], attr)
+        method.param_lines.append(attr)
+
+        if 'defaultValue' in p:
+            p['defaultValue'] = "'%s'" % (p['defaultValue'])
+        else:
+            p['defaultValue'] = None
+
+        param = "%s=%s" % (param_name, p['defaultValue'])
+
+        method.method_params.append(param)
+
+    return ', '.join(method.method_params)
 
 
 def make_method_comment(class_desc, method_desc):
@@ -74,3 +104,17 @@ def make_method_comment(class_desc, method_desc):
     if method_desc and method_desc != class_desc:
         method_comments.append(method_desc)
     return '        """%s"""' % ('; '.join(method_comments))
+
+def make_api_call_params(method):
+    """Format the parameters to the call() method in asterisk_rest_api, the
+    util module which handles HTTP requests to Asterisk."""
+    params = ["'%s'" % (method.path),
+              "http_method='%s'" % (method.http_method)]
+    if method.method_name:
+        params.append("api_method='%s'" % (method.method_name))
+    if method.method_params:
+        params.append("parameters=params")
+    if method.required_id:
+        params.append("object_id=self.object_id")
+
+    return ', '.join(params)
